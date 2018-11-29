e1566e89f25867b4dbebf7e6a157bdb5b7a22ccb
Ro Hit Verma <rhtvma@outlook.com>
2018-11-29 23:37:51 +0530

    bubbleSort
    bubbleSort implementation

---
+++ bubbleSort.js
@@ -0,0 +1,20 @@
+
+var array = [2,5,8,5,9,3,8];
+
+function swap(arr, index1, index2){
+    var temp = arr[index1];
+    arr[index1]=arr[index2];
+    arr[index2]=temp;
+}
+
+function bubbleSort(){
+    for(var i=0; i <=array.length; i ++){
+       for(var j=0; j<=array.length-i; j++){
+            if(array[j]>array[j+1]){
+            swap(array,j,j+1);
+      }
+    }
+  }
+}
+
+bubbleSort();

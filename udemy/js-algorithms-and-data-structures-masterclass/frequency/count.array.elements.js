4923e6d943fc44e6e718460388777632968a1f34
Rohit Verma <rohit.verma@vvdntech.in>
2021-05-11 15:10:17 +0530

    frequency counter

---
+++ udemy/js-algorithms-and-data-structures-masterclass/frequency/count.array.elements.js
@@ -0,0 +1,29 @@
+/************************************************************************
+* Char Count
+*   
+* Solution 1    
+* - Make object ot return ar end.
+* - Loop over array, for each elements
+* - if element is already in object then increment counter
+* - Return Object at end
+*************************************************************************/
+const arrayEl = ['a', 'a', 'a', 2, 3, 4, 2, 2, 3, 4, 2, 2, 4];
+
+function elementCount(arr) {
+    let result = {};
+    for (let el of arr) {
+        if (result[el] > 0) {
+            result[el]++;
+        } else {
+            result[el] = 1;
+        }
+    }
+    return result;
+}
+
+console.log(elementCount(arrayEl));
+/*************************************************************************
+ * Output
+ * { '2': 5, '3': 2, '4': 3, a: 3 }
+ *
+ *************************************************************************/
